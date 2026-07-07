2ff0a28c93d0eba40a15f885d7d4a4981995474e
dau teacher <dau@dau.edu>
2026-07-07 16:35:34 +0000

    Renamed Animal.java to Robot.java

--- src/main/java/org/springframework/samples/petclinic/owner/Robot.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Robot.java
@@ -42,8 +42,8 @@ import jakarta.persistence.Table;
  * @author Wick Dynex
  */
 @Entity
-@Table(name = "animals")
-public class Animal extends NamedEntity {
+@Table(name = "robots")
+public class Robot extends NamedEntity {
 
 	@Column
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
